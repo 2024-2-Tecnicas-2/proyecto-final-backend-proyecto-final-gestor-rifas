b39fbb223bd7544d318dc9023dad93f9de68bf60
JavierAlmonacid-10 <javialmonacidtorres@gmail.com>
2024-11-13 19:14:59 -0500

    Add files via upload

---
+++ Usuarios.java
@@ -0,0 +1,36 @@
+package com.mycompany.avanceproyecto;
+
+public class Usuarios {
+
+    private String nombre;
+    private int edad;
+
+    
+    public Usuarios(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    
+    public void mostrarInfo() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+    }
+}
